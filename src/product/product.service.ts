e32b7314b95e613caf38452a18068a7528c51426
Emanuel <123523520+emanueldias01@users.noreply.github.com>
2025-08-25 18:27:15 -0300

    create module prisma

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class ProductService {
+  constructor(private readonly prismaService: PrismaService) {}
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   create(createProductDto: CreateProductDto) {
     return 'This action adds a new product';
   }
@@ -16,6 +19,7 @@ export class ProductService {
     return `This action returns a #${id} product`;
   }
 
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   update(id: number, updateProductDto: UpdateProductDto) {
     return `This action updates a #${id} product`;
   }
